2c99e89e9286e267aec797d0b46fab3e6b40a793
ahaddadl <ahaddad@vocento.com>
2024-11-24 14:09:33 +0100

    Lab completado

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
 import "./App.css";
 import Counter from "./components/Counter";
-import ExponentTwo from "./components/ExponentTwo";
-import ExponentThree from "./components/ExponentThree";
-import ExponentFour from "./components/ExponentFour";
-import ExponentFive from "./components/ExponentFive";
-import ExponentSix from "./components/ExponentSix";
-
+// import ExponentTwo from "./components/ExponentTwo";
+// import ExponentThree from "./components/ExponentThree";
+// import ExponentFour from "./components/ExponentFour";
+// import ExponentFive from "./components/ExponentFive";
+// import ExponentSix from "./components/ExponentSix";
+import Exponent from "./components/Exponent";
+import { useState } from "react";
 
 function App () {
+  const [count, setCount] = useState(0);
+
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);
+
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
   
-      <Counter/>
+      <Counter count={count} increment={increment} decrement={decrement}/>
 
       <br />
       <h2><em>Exponents</em></h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        {[2, 3, 4, 5, 6].map((exponent) => (
+          <Exponent key={exponent} num={count} exponent={exponent} />
+        ))}
       </div>
+
+      {/* <div className="container">
+        <ExponentTwo count={count}/>
+        <ExponentThree count={count}/>
+        <ExponentFour count={count}/>
+        <ExponentFive count={count}/>
+        <ExponentSix count={count}/>
+      </div> */}
     </div>
   );
 }
